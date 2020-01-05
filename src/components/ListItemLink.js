2882d284385fa79a5edd18631f4f767bba28133b
Mitchell Falkow <mdfalkow@gmail.com>
2020-01-05 14:54:40 -0500

    basic display

---
+++ src/components/ListItemLink.js
@@ -0,0 +1,25 @@
+import React, { useMemo, forwardRef } from 'react'
+// import PropTypes from 'prop-types'
+import { Link as GatsbyLink } from 'gatsby'
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+
+export default ({ icon, primary, primaryProps, to }) => {
+  const renderLink = useMemo(
+    () => forwardRef((itemProps, ref) => <GatsbyLink to={to} ref={ref} {...itemProps} />),
+    [to]
+  )
+
+  return (
+    <ListItem button component={renderLink}>
+      {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+      <ListItemText primary={primary} {...primaryProps}/>
+    </ListItem>
+  )
+}
+
+// ListItemLink.propTypes = {
+//   icon: PropTypes.element,
+//   primary: PropTypes.string.isRequired,
+//   to: PropTypes.string.isRequired,
+//   primaryProps: PropTypes.object
+// }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,81 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import {
+  Grid,
+  makeStyles,
+  Paper,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme
+} from '@material-ui/core'
+import ListItemLink from '../components/ListItemLink'
+import theme from '../styles/theme'
 
-export default () => (
-  <Typography variant={'h1'}>
-    Hello
-  </Typography>
+const useStyles = makeStyles(theme => ({
+  root: {
+    [theme.breakpoints.up('md')]: {
+      height: '100%'
+    }
+  }
+}))
+
+const baseContainerProps = {
+  alignItems: 'center',
+  justify: 'center',
+  style: {
+    padding: 16
+  }
+}
+
+const Title = ({ className }) => (
+  <Grid container item xs={12} md={6} {...baseContainerProps} className={className}>
+    <Grid container item xs={11}>
+      <Grid item xs={12}>
+        <Typography variant={'h1'}>Mitchell Falkow</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant={'h3'}>Full-Stack Developer</Typography>
+        <Typography variant={'h3'}>Machine Learning Enthusiast</Typography>
+      </Grid>
+    </Grid>
+  </Grid>
 )
+
+const Content = ({ className }) => {
+
+  const sections = ['About', 'Projects', 'Blog', 'Resume', 'Contact']
+
+  return (
+    <Grid container item xs={12} md={6} {...baseContainerProps}>
+      <Grid item xs={10}>
+        <Paper>
+          <List>
+            {sections.map(section =>
+              <ListItemLink
+                to={`/${section}`}
+                icon={null}
+                primary={section}
+                primaryProps={{ primaryTypographyProps: { variant: 'h4' } }}/>
+            )}
+          </List>
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+}
+
+
+export default () => {
+  const theme = useTheme()
+  const classes = useStyles(theme)
+  return (
+    <>
+      <Grid container className={classes.root}>
+        <Title/>
+        <Content/>
+      </Grid>
+    </>
+  )
+}
