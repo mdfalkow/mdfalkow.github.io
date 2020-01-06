4d94a4eb266faabaa3513d7b061e77664ab248b0
Mitchell Falkow <mdfalkow@gmail.com>
2020-01-06 16:21:22 -0500

    home layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, makeStyles, MuiThemeProvider } from '@material-ui/core'
+import { Box, CssBaseline, Grid, makeStyles, MuiThemeProvider, Typography } from '@material-ui/core'
 import theme from '../styles/theme'
 import { Helmet } from 'react-helmet/es/Helmet'
 
@@ -20,6 +20,7 @@ export default ({ children }) => {
       <Helmet>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       </Helmet>
+      {/*<CssBaseline/>*/}
       <MuiThemeProvider theme={theme}>
         <Box className={classes.root}>
           {children}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,8 @@ import {
   makeStyles,
   Paper,
   List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
   Typography,
-  useTheme
+  useTheme, Box, Container
 } from '@material-ui/core'
 import ListItemLink from '../components/ListItemLink'
 import theme from '../styles/theme'
@@ -23,24 +20,28 @@ const useStyles = makeStyles(theme => ({
 
 const baseContainerProps = {
   alignItems: 'center',
-  justify: 'center',
-  style: {
-    padding: 16
-  }
+  justify: 'center'
 }
 
 const Title = ({ className }) => (
-  <Grid container item xs={12} md={6} {...baseContainerProps} className={className}>
-    <Grid container item xs={11}>
-      <Grid item xs={12}>
-        <Typography variant={'h1'}>Mitchell Falkow</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Typography variant={'h3'}>Full-Stack Developer</Typography>
-        <Typography variant={'h3'}>Machine Learning Enthusiast</Typography>
-      </Grid>
+  <Grid container item {...baseContainerProps}>
+    <Grid item xs={10}>
+      <Typography variant={'h1'}>Mitchell Falkow</Typography>
+      <Typography variant={'h3'}>Full-Stack Developer</Typography>
+      <Typography variant={'h3'}>Machine Learning Enthusiast</Typography>
     </Grid>
   </Grid>
+  // <Grid container item xs={12} md={6} {...baseContainerProps} className={className}>
+  // <Grid container item xs={11}>
+  // <Grid item xs={12}>
+  // <Typography variant={'h1'}>Mitchell Falkow</Typography>
+  // </Grid>
+  // <Grid item xs={12}>
+  // <Typography variant={'h3'}>Full-Stack Developer</Typography>
+  // <Typography variant={'h3'}>Machine Learning Enthusiast</Typography>
+  // </Grid>
+  // </Grid>
+  // </Grid>
 )
 
 const Content = ({ className }) => {
@@ -48,18 +49,16 @@ const Content = ({ className }) => {
   const sections = ['About', 'Projects', 'Blog', 'Resume', 'Contact']
 
   return (
-    <Grid container item xs={12} md={6} {...baseContainerProps}>
+    <Grid container item {...baseContainerProps}>
       <Grid item xs={10}>
-        <Paper>
-          <List>
-            {sections.map(section =>
-              <ListItemLink
-                to={`/${section}`}
-                icon={null}
-                primary={section}
-                primaryProps={{ primaryTypographyProps: { variant: 'h4' } }}/>
-            )}
-          </List>
+        <Paper component={List}>
+          {sections.map(section =>
+            <ListItemLink
+              to={`/${section}`}
+              icon={null}
+              primary={section}
+              primaryProps={{ primaryTypographyProps: { variant: 'h4' } }}/>
+          )}
         </Paper>
       </Grid>
     </Grid>
@@ -73,8 +72,13 @@ export default () => {
   return (
     <>
       <Grid container className={classes.root}>
-        <Title/>
-        <Content/>
+        <Grid container item xs={12} md={6}>
+          <Title/>
+        </Grid>
+        {/*<Grid item xs={false} md={6}/>*/}
+        <Grid container item xs={12} md={6}>
+          <Content/>
+        </Grid>
       </Grid>
     </>
   )
